8c3bf2626b2d3d5da19540a18f52109c2708aa29
Prapti Mahehswari <67674408+Prapti08@users.noreply.github.com>
2021-11-11 20:07:08 +0530

    Update settings.py
    2nd

--- quarantine/quarantine/settings.py
+++ quarantine/quarantine/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = 'yfbiehk12x@01j9ovvg@l2d=-5sit*$*$&p(@t280y9#k%i6gc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','covid-deff.herokuapp.com']
 
 
 # Application definition
